d46031252831a0461b93d66323a2ca020cb9f825
mitskkmatsveii <mat.mitsko@gmail.com>
2026-05-28 15:21:00 +0300

    check.py

--- api/check.py
+++ api/check.py
@@ -1,21 +1,12 @@
-from http.server import BaseHTTPRequestHandler
-from urllib.parse import urlparse, parse_qs
-
-VALID_KEYS = [
-    "PREM-A1B2-C3D4",
-    "PREM-X9Y8-Z7W6",
-]
-
-class handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        query = parse_qs(urlparse(self.path).query)
-        key = query.get("key", [""])[0]
-        
-        if key in VALID_KEYS:
-            response = b"valid"
-        else:
-            response = b"invalid"
-            
-        self.send_response(200)
-        self.end_headers()
-        self.wfile.write(response)
+def handler(request):
+    key = request.args.get("key", "")
+    
+    VALID_KEYS = [
+        "PREM-A1B2-C3D4",
+        "PREM-X9Y8-Z7W6",
+    ]
+    
+    if key in VALID_KEYS:
+        return "valid"
+    else:
+        return "invalid"
